f6a39f789ff3fbc2fc329eba25df5446da0774e5
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2025-09-09 00:03:56 -0500

    Se especifico API REST siguiendo principios OpenApi/Swagger de ejemplo

--- src/main/java/com/coworking/application/coworking_booking/config/SwaggerConfig.java
+++ src/main/java/com/coworking/application/coworking_booking/config/SwaggerConfig.java
@@ -1,4 +1,52 @@
 package com.coworking.application.coworking_booking.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Value("${server.port:8080}")
+    private String serverPort;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Coworking Booking API")
+                        .description("API para gestión de reservas de espacios de coworking")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Equipo de Desarrollo")
+                                .email("dev@coworking.com")
+                                .url("https://coworking.com"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:" + serverPort)
+                                .description("Servidor de Desarrollo"),
+                        new Server()
+                                .url("https://api.coworking.com")
+                                .description("Servidor de Producción")))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Ingrese el token JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
 }
